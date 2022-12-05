f1b0ccf80497a2f839b3c4907643f622449557f7
[Ismael] <ismaelgomez1986@gmail.com>
2022-12-05 19:50:22 -0300

    minor fixes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ function NavBar({ img, children }) {
     return (
         <div className="navbar bg-base-300">
             <div className="flex-1 mx-auto">
-                <img src={img} className="h-8 mr-3 sm:h-9" />
+                <img src={img} className="h-8 mr-3 sm:h-9 md:h-10" />
                 <a className="btn btn-ghost normal-case text-xl italic">ChessWorld Store</a>
                 <div>{children}</div>
             </div>
@@ -16,15 +16,15 @@ function NavBar({ img, children }) {
                 </ul>
             </div>
             <div className="dropdown navbar-end ml-20">
-                <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                </label>
                 <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-300 rounded-box w-52">
                     <li><a>Home</a></li>
                     <li><a>Prices</a></li>
                     <li><a>Contact</a></li>
                     <li><a>About Us</a></li>
                 </ul>
+                <label tabIndex={0} className="btn btn-ghost lg:hidden navbar-end">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                </label>
             </div>
         </div>
     )
